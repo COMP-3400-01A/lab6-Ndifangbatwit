0c59430c13fb3db5a619307b311923d07017ff94
Ndifangbatwit <ndifangb@wit.edu>
2026-08-01 14:23:43 -0400

    Done

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ CFLAGS += -Wall -Wextra -Wfatal-errors -g3
 CFLAGS += -Werror=vla -Werror=shadow -Wno-unused -Wno-unused-parameter
 CFLAGS += -fsanitize=address -fsanitize=undefined
 
-all: test_parta_init test_parta_run_proc test_parta_fcfs test_parta_rr_next test_parta_rr
+all: parta_main test_parta_init test_parta_run_proc test_parta_fcfs test_parta_rr_next test_parta_rr
+
+parta_main: parta_main.c parta.c
+	$(CC) $(CFLAGS) -o parta_main parta_main.c parta.c
 
 test_parta_init: parta.c unity.c test_parta_init.c
 	$(CC) $(CFLAGS) -o test_parta_init parta.c unity.c test_parta_init.c
@@ -21,4 +24,4 @@ test_parta_rr: parta.c unity.c test_parta_rr.c
 
 .PHONY: clean
 clean:
-	rm -rf test_parta_init test_parta_run_proc test_parta_fcfs test_parta_rr_next test_parta_rr
+	rm -rf parta_main test_parta_init test_parta_run_proc test_parta_fcfs test_parta_rr_next test_parta_rr
--- parta_main.c
+++ parta_main.c
@@ -5,6 +5,85 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argc < 3) {
+        printf("ERROR: Missing arguments\n");
+        return 1;
+    }
+
+     int *bursts;
+    int count;
+    int quantum = 0;
+
+    if (strcmp(argv[1], "fcfs") == 0) {
+
+        if (argc < 3) {
+            printf("ERROR: Missing arguments\n");
+            return 1;
+        }
+
+        printf("Using FCFS\n\n");
+
+        count = argc - 2;
+        bursts = malloc(sizeof(int) * count);
+
+        for (int i = 0; i < count; i++) {
+            bursts[i] = atoi(argv[i + 2]);
+            printf("Accepted P%d: Burst %d\n", i, bursts[i]);
+        }
+
+        struct pcb *procs = init_procs(bursts, count);
+
+        fcfs_run(procs, count);
+
+        double total = 0;
+        for (int i = 0; i < count; i++)
+            total += procs[i].wait;
+
+        printf("Average wait time: %.2f\n", total / count);
+
+        free(procs);
+        free(bursts);
+    }
+
+     else if (strcmp(argv[1], "rr") == 0) {
+
+        if (argc < 4) {
+            printf("ERROR: Missing arguments\n");
+            return 1;
+        }
+
+        quantum = atoi(argv[2]);
+
+        printf("Using RR(%d)\n\n", quantum);
+
+        count = argc - 3;
+        bursts = malloc(sizeof(int) * count);
+
+        for (int i = 0; i < count; i++) {
+            bursts[i] = atoi(argv[i + 3]);
+            printf("Accepted P%d: Burst %d\n", i, bursts[i]);
+        }
+
+        struct pcb *procs = init_procs(bursts, count);
+
+        rr_run(procs, count, quantum);
+
+        double total = 0;
+
+        for (int i = 0; i < count; i++)
+            total += procs[i].wait;
+
+        printf("Average wait time: %.2f\n", total / count);
+
+        free(procs);
+        free(bursts);
+    }
+
+     else {
+        printf("ERROR: Missing arguments\n");
+        return 1;
+    }
+
     return 0;
+
 }
